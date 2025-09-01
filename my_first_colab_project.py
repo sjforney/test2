f37a9e3d7f93c11f929ff54207d8d28126305060
your-name-here <your-email-here@example.com>
2025-09-01 05:13:15 +0000

    Add new goodbye function

--- my_first_colab_project.py
+++ my_first_colab_project.py
@@ -3,3 +3,10 @@ def hello_world():
     print('Hello, GitHub!')
 
 hello_world()
+
+
+# This is a new function added in the next commit!
+def say_goodbye():
+    print('Goodbye, GitHub!')
+
+say_goodbye()
